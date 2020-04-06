adb31584806ddedb2ab9b4eeab6dee4b69cd0c03
tand826 <takumi.ando826@gmail.com>
2020-04-06 14:34:28 +0900

    update views

--- main.py
+++ main.py
@@ -31,13 +31,19 @@ class Application(ttk.Frame):
         )
         self.selected_files.bind(
             '<ButtonRelease-1>', self.show_thumb_when_selected)
-        self.selected_files.grid(column=0, row=1, rowspan=11, sticky=tk.N+tk.S)
+        self.selected_files.grid(
+            column=0, row=1, rowspan=11, sticky=tk.N+tk.S, padx=3)
 
         self.add_combobox(
-            "method", ["none", "classification", "detection", "segmentation"], col=1, row=0)
-        self.add_dialog_box("where to save", self.select_one_directory, col=1, row=1)
-        self.add_dialog_box("annotation file", self.select_one_file, col=1, row=2)
-        self.add_dialog_box("inclusion file", self.select_one_file, col=1, row=3)
+            "method",
+            ["none", "classification", "detection", "segmentation"],
+            col=1, row=0)
+        self.add_dialog_box(
+            "where to save", self.select_one_directory, col=1, row=1)
+        self.add_dialog_box("annotation file",
+                            self.select_one_file, col=1, row=2)
+        self.add_dialog_box(
+            "inclusion file", self.select_one_file, col=1, row=3)
         self.add_param_box("patch width", col=1, row=4)
         self.add_param_box("patch height", col=1, row=5)
         self.add_param_box("overlap width", col=1, row=6)
@@ -46,14 +52,16 @@ class Application(ttk.Frame):
         self.add_param_box("ratio of annotation", col=1, row=9)
         self.add_combobox("magnification", ["40", "20", "10"], col=1, row=10)
         self.add_run_button(col=1, row=11)
-        self.add_status_bar(col=0, row=12)
+        # self.add_status_bar(col=2, row=12)
 
     def add_dialog_box(self, param_name, callback, col, row):
-        self.labels[param_name] = ttk.Label(
+        self.labels[param_name] = ttk.Entry(
             self,
             textvariable=self.params[param_name]
         )
-        self.labels[param_name].grid(column=col+1, row=row)
+        self.labels[param_name].grid(
+            column=col+1, row=row, sticky=tk.E+tk.W)
+
         ttk.Button(
             self,
             text="{}".format(param_name),
@@ -97,7 +105,7 @@ class Application(ttk.Frame):
         ttk.Entry(
             self,
             textvariable=self.params[param_name]
-        ).grid(column=col+1, row=row)
+        ).grid(column=col+1, row=row, sticky=tk.E+tk.W)
 
     def add_combobox(self, param_name, dropdowns, col, row):
         ttk.Label(
@@ -160,24 +168,26 @@ class Application(ttk.Frame):
 
     def show_thumb_when_selected(self, event):
         return  # no operation for now
-        #idx = self.selected_files.curselection()[0]
-        #filepath = self.selected_files_list[idx]
-        #slide = wp.slide(filepath)
-        #thumbname = "{}.png".format(Path(filepath).stem)
-        #slide.get_thumbnail(256).pngsave(thumbname)
-        #image = ImageTk.PhotoImage(file=thumbname)
-        #self.thumbnail_label = ttk.Label(
+        # idx = self.selected_files.curselection()[0]
+        # filepath = self.selected_files_list[idx]
+        # slide = wp.slide(filepath)
+        # thumbname = "{}.png".format(Path(filepath).stem)
+        # slide.get_thumbnail(256).pngsave(thumbname)
+        # image = ImageTk.PhotoImage(file=thumbname)
+        # self.thumbnail_label = ttk.Label(
         #    self.master,
         #    image=image
-        #)
-        #self.thumbnail_label.pack(side="top")
+        # )
+        # self.thumbnail_label.pack(side="top")
 
     def set_window(self):
-        self.master.geometry("515x327")
+        self.master.geometry("521x315")
         self.master.title("WSI Patcher")
-        self.master.configure(bg="white")
-        # self.selected_files_scrollbar = tk.Scrollbar(self)
-        # self.selected_files_scrollbar.pack(fill="y")
+        self.master.configure(bg="#ECECEC")
+        self.master.resizable(0, 0)
+        self.master.columnconfigure(0, weight=1)
+        self.master.columnconfigure(1, weight=1)
+        self.master.columnconfigure(2, weight=1)
 
     def set_types(self):
         self.types = [
